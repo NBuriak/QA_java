bb7bbc9fa42643f2b32dc81b4e4675494a3206ef
NBuriak <st.rodger86@gmail.com>
2022-02-02 16:06:21 +0300

    новые уроки

---
+++ sandbox/src/main/java/ru/QA/sandbox/Point.java
@@ -0,0 +1,13 @@
+package ru.QA.sandbox;
+
+public class Point {
+    public double x1;
+    public double x2;
+    public Point (double x1, double x2){
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+    public  double area(){
+        return x2-x1;
+    }
+}
